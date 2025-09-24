c05cba464c32c9d0c5d2e5655c58dfcd29979fc7
Leandro Andriani <landriani@alumno.unlam.edu.ar>
2025-09-24 16:47:30 -0300

    Agregado cambio en controllerLogin

--- src/main/java/com/tallerwebi/presentacion/ControladorLogin.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorLogin.java
@@ -38,6 +38,7 @@ public class ControladorLogin {
         Usuario usuarioBuscado = servicioLogin.consultarUsuario(datosLogin.getEmail(), datosLogin.getPassword());
         if (usuarioBuscado != null) {
             request.getSession().setAttribute("ROL", usuarioBuscado.getRol());
+            request.getSession().setAttribute("id", usuarioBuscado.getId());
             request.getSession().setAttribute("USUARIO", usuarioBuscado);
             return new ModelAndView("redirect:/home");
         } else {
