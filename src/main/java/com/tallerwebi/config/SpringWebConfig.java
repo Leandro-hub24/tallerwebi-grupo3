0bfaa0397818d54353ac67592aa52bfae2ff8d18
Leandro Andriani <landriani@alumno.unlam.edu.ar>
2025-09-24 22:32:47 -0300

    Cambios en home y reubicacion de imagenes,

--- src/main/java/com/tallerwebi/config/SpringWebConfig.java
+++ src/main/java/com/tallerwebi/config/SpringWebConfig.java
@@ -27,19 +27,13 @@ public class SpringWebConfig implements WebMvcConfigurer {
         registry.addResourceHandler("/img/**").addResourceLocations("/resources/core/img/");
         registry.addResourceHandler("/css/**").addResourceLocations("/resources/core/css/");
         registry.addResourceHandler("/js/**").addResourceLocations("/resources/core/js/");
-        registry.addResourceHandler("/imag/**").addResourceLocations("/webapp/img/versus/");
         registry.addResourceHandler("/webjars/**").addResourceLocations("/webjars/");
         // Static resources (images) under src/main/webapp/resources/**
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-        registry.addResourceHandler("/img/**")
-                .addResourceLocations("file:src/main/webapp/img/")
-                .setCachePeriod(0);
 
-        // Para recursos en src/main/webapp/resources/
-        registry.addResourceHandler("/resources/**")
-                .addResourceLocations("file:src/main/webapp/resources/")
-                .setCachePeriod(0);
     }
+
+
 
     // https://www.thymeleaf.org/doc/tutorials/3.0/thymeleafspring.html
     // Spring + Thymeleaf
