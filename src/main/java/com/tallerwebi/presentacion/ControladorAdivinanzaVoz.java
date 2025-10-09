b65417432aa94d938201b4c70947202bc925b0a3
t4k020 <t4k020@gmail.com>
2025-10-09 14:47:15 -0300

    limpieza de codigo

--- src/main/java/com/tallerwebi/presentacion/ControladorAdivinanzaVoz.java
+++ src/main/java/com/tallerwebi/presentacion/ControladorAdivinanzaVoz.java
@@ -1,5 +1,8 @@
 package com.tallerwebi.presentacion;
 
+import com.tallerwebi.dominio.ServicioAdivinanza;
+import com.tallerwebi.dominio.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +11,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
 import java.util.*;
 
 @Controller
 @RequestMapping("/adivinanza-por-voz")
 public class ControladorAdivinanzaVoz {
+    private ServicioAdivinanza servicioAdivinanza;
 
     // Mapa de imágenes y sus nombres (sin extensión)
     private final Map<String, String> imagenes = new LinkedHashMap<>() {{
@@ -28,6 +33,12 @@ public class ControladorAdivinanzaVoz {
         put("tralalero", "Tralalelo Tralala");
         put("tuntuntunsahur", "Tuntuntun Sahur");
     }};
+
+    @Autowired
+    public ControladorAdivinanzaVoz(ServicioAdivinanza servicioAdivinanza) {
+        this.servicioAdivinanza = servicioAdivinanza;
+
+    }
 
     // GET: muestra la vista con la imagen y formulario
     @GetMapping("")
@@ -49,10 +60,16 @@ public class ControladorAdivinanzaVoz {
     public ModelAndView verificarPorVoz(
             @RequestParam String transcripcion,
             @RequestParam String imagenActual,
-            @RequestParam(required = false) String aliasDetectado){
-
+            @RequestParam(required = false) String aliasDetectado,
+            HttpSession session){
+        Usuario usuario = (Usuario) session.getAttribute("usuario");
         String respuestaCorrecta = imagenes.get(imagenActual);
         boolean esCorrecto = transcripcion.equalsIgnoreCase(respuestaCorrecta);
+        if (esCorrecto){
+            servicioAdivinanza.opcionCorrecta( usuario);
+        }else{
+            servicioAdivinanza.opcionIncorrecta(usuario);
+        }
 
         System.out.println("🧠 Transcripción recibida: " + transcripcion);
         System.out.println("🎯 Alias detectado: " + aliasDetectado);
--- src/test/java/com/tallerwebi/presentacion/ControladorAdivinanzaPorVozTest.java
+++ src/test/java/com/tallerwebi/presentacion/ControladorAdivinanzaPorVozTest.java
@@ -1,23 +1,31 @@
 package com.tallerwebi.presentacion;
 
+import com.tallerwebi.dominio.ServicioAdivinanza;
 import org.junit.jupiter.api.Test;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpSession;
 
 import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.mockito.Mockito.mock;
 
 public class ControladorAdivinanzaPorVozTest {
+    private ServicioAdivinanza servicioAdivinanzaMock;
+
 
     @Test
     public void queVerificaRespuestaCorrecta() {
-        ControladorAdivinanzaVoz controlador = new ControladorAdivinanzaVoz();
+        HttpSession session = mock(HttpSession.class);
+        servicioAdivinanzaMock =  mock(ServicioAdivinanza.class);
+        ControladorAdivinanzaVoz controlador = new ControladorAdivinanzaVoz(servicioAdivinanzaMock);
         // Configuración
         String nombreArchivo = "Tuntuntun Sahur";
         String textoReconocido = "Tuntuntun Sahur";
 
         // Ejecución
-        ModelAndView mav = controlador.verificarPorVoz(textoReconocido, nombreArchivo, "alias");
+        ModelAndView mav = controlador.verificarPorVoz(textoReconocido, nombreArchivo, "alias", session);
 
         // Verificación
         assertThat(mav.getViewName(), equalTo("verificar2"));
