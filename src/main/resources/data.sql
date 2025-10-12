2c44f6dd5d9328a0674db53f5ce01795efc4f13e
Leandro Andriani <landriani@alumno.unlam.edu.ar>
2025-10-12 17:32:14 -0300

    Agregado niveles de Rompecabezas

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,11 @@ INSERT INTO NivelJuego(id, nombre, nivel, usuario_id) VALUES (null, 'Rompecabeza
 INSERT INTO Rompecabeza(id, nombre, urlImg) VALUES (null, 'Bellerina-cappuccina', '/img/versus/pjpngs/bellerina-cappuccina.png');
 INSERT INTO Rompecabeza(id, nombre, urlImg) VALUES (null, 'Bombardino-crocodilo', '/img/versus/pjpngs/bombardino-crocodilo.png');
 INSERT INTO Rompecabeza(id, nombre, urlImg) VALUES (null, 'Brrbrrr-patapi', '/img/versus/pjpngs/brrbrrr-patapi.png');
+INSERT INTO Rompecabeza(id, nombre, urlImg) VALUES (null, 'Capiballero-cocosini', '/img/versus/pjpngs/capiballero-cocosini.png');
+INSERT INTO Rompecabeza(id, nombre, urlImg) VALUES (null, 'Capuccino-assasno', '/img/versus/pjpngs/capuccino-assasno.png');
+INSERT INTO Rompecabeza(id, nombre, urlImg) VALUES (null, 'Chimpancini-bananini', '/img/versus/pjpngs/chimpancini-bananini.png');
+INSERT INTO Rompecabeza(id, nombre, urlImg) VALUES (null, 'Liriririlarila', '/img/versus/pjpngs/liriririlarila.png');
+INSERT INTO Rompecabeza(id, nombre, urlImg) VALUES (null, 'Los-tralaleritos', '/img/versus/pjpngs/los-tralaleritos.png');
+INSERT INTO Rompecabeza(id, nombre, urlImg) VALUES (null, 'Saturno-saturnita', '/img/versus/pjpngs/saturno-saturnita.png');
+INSERT INTO Rompecabeza(id, nombre, urlImg) VALUES (null, 'Tralalero', '/img/versus/pjpngs/tralalero.png');
+INSERT INTO Rompecabeza(id, nombre, urlImg) VALUES (null, 'Tuntuntunsahur', '/img/versus/pjpngs/tuntuntunsahur.png');
