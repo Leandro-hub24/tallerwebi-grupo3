29d2779d1630969dfb0ffdb55d3e99ea2b017d6d
Leandro Andriani <landriani@alumno.unlam.edu.ar>
2025-11-06 17:30:12 -0300

    Refactorizacion de test rompecabeza multijugador

--- src/main/java/com/tallerwebi/dominio/ServicioPartida.java
+++ src/main/java/com/tallerwebi/dominio/ServicioPartida.java
@@ -9,6 +9,9 @@ import java.util.Collection;
 public interface ServicioPartida {
 
         Partida crearPartida(String nombrePartida, Integer usuario1Id, String username, String juego);
+        void setPartidasEnCurso(String idPartida, Partida partida);
+        void setPartidasAbiertas(String idPartida, Partida partida);
+        void setPartidasTerminadas(String idPartida, Partida partida);
         Collection<Partida> getPartidasAbiertas();
         Partida unirJugador(String partidaId, Integer usuario2Id, String username, String juego) throws PartidaNoEncontradaException, PartidaLlenaException;
         void terminarPartida(String partidaId, Integer usuarioId, String juego);
